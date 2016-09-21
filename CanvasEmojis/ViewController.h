4d07ec195ef776fcc1922babcb94bd9c72373da5
Yash Kshirsagar <yashk@yahoo-inc.com>
2016-09-21 10:12:15 -0700

    Initial Commit

---
+++ CanvasEmojis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CanvasEmojis
+//
+//  Created by Yash Kshirsagar on 9/21/16.
+//  Copyright © 2016 Yash Kshirsagar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
